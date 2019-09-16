c111586ccd3a8e1f1796cd43d847eb3616d43573
LAPTOP-C403633C\Dika Handika <dika.csui@gmail.com>
2019-09-16 14:33:31 +0700

    deploy

---
+++ precache-manifest.155725c5c75bad5a32595e3516bce2aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "75c38e1280ff8265939318458cdbe9f5",
+    "url": "/foreign-exchange-currency/style.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/foreign-exchange-currency/robots.txt"
+  },
+  {
+    "revision": "22480a61890427c974cf",
+    "url": "/foreign-exchange-currency/js/chunk-vendors.e856292f.js"
+  },
+  {
+    "revision": "43c682ad887037f8ce68",
+    "url": "/foreign-exchange-currency/js/app.f8ad7208.js"
+  },
+  {
+    "revision": "f1123c66b38c7ee8df49a52e527faf7b",
+    "url": "/foreign-exchange-currency/index.html"
+  },
+  {
+    "revision": "d5b711e94d3cc893ef6f500eb1025176",
+    "url": "/foreign-exchange-currency/img/trash.d5b711e9.svg"
+  },
+  {
+    "revision": "43c682ad887037f8ce68",
+    "url": "/foreign-exchange-currency/css/app.321e9328.css"
+  }
+];
